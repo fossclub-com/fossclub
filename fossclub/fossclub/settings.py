ab3c99ae5aec66c3127af28b3214ab2cd28c8502
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2022-01-22 11:41:15 +0530

    Add accounts thingy

--- fossclub/fossclub/settings.py
+++ fossclub/fossclub/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = "fossclub.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [str(BASE_DIR / "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
