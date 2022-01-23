8df3f20e424e6cf9cbe48bf4040b577d322e17ce
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2022-01-23 12:48:12 +0530

    Add perks claim

--- fossclub/perks/urls.py
+++ fossclub/perks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.list_perks, name="list_perks"),
     path("badges/", views.list_badges, name="list_badges"),
     path("new/", views.PerkCreateView.as_view(), name="create_perk"),
-    path("<int:perk_id>/", views.show_perk, name="show_perk")
+    path("<int:perk_id>/", views.show_perk, name="show_perk"),
+    path("claim/<int:perk_id>/", views.claim_perk, name="claim_perk"),
 ]
