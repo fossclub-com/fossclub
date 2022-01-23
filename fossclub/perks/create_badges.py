2e48fe0b478d3fc4b8e51dc41cda88880e7d4eab
Shardul Kurdukar <24shardul@gmail.com>
2022-01-23 10:58:01 +0530

    Editted styles and badges file

--- fossclub/perks/create_badges.py
+++ fossclub/perks/create_badges.py
@@ -13,6 +13,6 @@ badges = [
     {"name": "GitLab Signin", "image": "badges/gitlab_login.png", "short_description": "Have a GitLab account", "max_progress": 1, "unit": "GitLab OAuth"},
     {"name": "Tensorflow Contributor", "image": "badges/tensorflow_contributor.png", "short_description": "Have contributed to Tensorflow", "max_progress": 1, "unit": "commits in tensorflow/tensorflow"},
 ]
-
+Badge.objects.all().delete()
 for badge_args in badges:
   Badge.objects.get_or_create(**badge_args)
